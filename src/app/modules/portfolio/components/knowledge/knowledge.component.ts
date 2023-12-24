1f9261f3ea36fa5059bd627e25ab357152ea0c3f
Guilherme de Souza Fornaciari <FornaciariWebDev@gmail.com>
2023-12-24 11:43:36 -0400

    updated knowledge components

--- src/app/modules/portfolio/components/knowledge/knowledge.component.ts
+++ src/app/modules/portfolio/components/knowledge/knowledge.component.ts
@@ -13,12 +13,30 @@ import { IKnowledge } from '../../interface/IKnowledge.interface';
 export class KnowledgeComponent {
   public arrayKnowledge = signal<IKnowledge[]>([
     {
-      src: 'assets/icons/knowledge/angular.svg',
+      src: 'assets/icons/knowledge/angular.ico',
       alt: 'Ícone de conhecimento de angular',
     },
     {
-      src: 'assets/icons/knowledge/nodejs.svg',
+      src: 'assets/icons/knowledge/node.png',
       alt: 'Ícone de conhecimento de nodejs',
+    },
+    {
+      src: 'assets/icons/knowledge/nestJS.svg',
+      alt: 'Ícone de conhecimento de NestJS',
+    },    {
+      src: 'assets/icons/knowledge/django.png',
+      alt: 'Ícone de conhecimento de Django',
+    },
+    {
+      src: 'assets/icons/knowledge/dotnet.png',
+      alt: 'Ícone de conhecimento de DotNet',
+    },    {
+      src: 'assets/icons/knowledge/FastReport.png',
+      alt: 'Ícone de conhecimento de FastReport',
+    },
+    {
+      src: 'assets/icons/knowledge/Pandas.png',
+      alt: 'Ícone de conhecimento de Pandas',
     },
   ]);
 }
