6054b5ec7c6db99a465bbddc4c3fefaf0060e00c
Guilherme de Souza Fornaciari <FornaciariWebDev@gmail.com>
2023-12-24 14:31:25 -0400

    Added github and a link to every knowledge

--- src/app/modules/portfolio/components/knowledge/knowledge.component.ts
+++ src/app/modules/portfolio/components/knowledge/knowledge.component.ts
@@ -15,28 +15,35 @@ export class KnowledgeComponent {
     {
       src: 'assets/icons/knowledge/angular.ico',
       alt: 'Ícone de conhecimento de angular',
+      href: 'https://angular.dev/',
     },
     {
       src: 'assets/icons/knowledge/node.png',
       alt: 'Ícone de conhecimento de nodejs',
+      href: 'https://nodejs.org/en/about',
     },
     {
       src: 'assets/icons/knowledge/nestJS.svg',
       alt: 'Ícone de conhecimento de NestJS',
+      href: 'https://nestjs.com/',
     },    {
       src: 'assets/icons/knowledge/django.png',
       alt: 'Ícone de conhecimento de Django',
+      href: 'https://www.djangoproject.com/',
     },
     {
       src: 'assets/icons/knowledge/dotnet.png',
       alt: 'Ícone de conhecimento de DotNet',
+      href: 'https://dotnet.microsoft.com/pt-br/',
     },    {
       src: 'assets/icons/knowledge/FastReport.png',
       alt: 'Ícone de conhecimento de FastReport',
+      href: 'https://github.com/FastReports/FastReport'
     },
     {
       src: 'assets/icons/knowledge/Pandas.png',
       alt: 'Ícone de conhecimento de Pandas',
+      href: 'https://pandas.pydata.org/'
     },
   ]);
 }
--- src/app/modules/portfolio/interface/IKnowledge.interface.ts
+++ src/app/modules/portfolio/interface/IKnowledge.interface.ts
@@ -1,4 +1,5 @@
 export interface IKnowledge {
   src: string;
   alt: string;
+  href: string;
 }
