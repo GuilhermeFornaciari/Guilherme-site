b92f54f81991d9ac1b59f58e7c584e95029821e4
Guilherme de Souza Fornaciari <fornaciari049@gmail.com>
2024-04-26 23:33:33 -0300

    added new w and h values

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -27,8 +27,8 @@ export class ProjectsComponent {
         src: 'assets/img/sosa.png',
         alt: 'SOSA',
         title: 'SOSA',
-        with: '360',
-        height: '160',
+        with: '330',
+        height: '140',
         description: 'Esse projeto foi desenvolvido para a CAED do IFRO Campus Cacoal em colaboração com Julio César Aguiar, vide GitHub para mais informações',
         links: [
           {
@@ -41,8 +41,8 @@ export class ProjectsComponent {
         src: 'assets/img/urflow.png',
         alt: 'UR-Flow',
         title: 'UR-Flow',
-        with: '360',
-        height: '160',
+        with: '330',
+        height: '140',
         description: 'Esse projeto foi desenvolvido para a CAED do IFRO Campus Cacoal em colaboração com Julio César Aguiar, vide GitHub para mais informações',
         links: [
           {
